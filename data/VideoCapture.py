553e5e39aa6a04c4dd036e5272e17918a72fe71f
Dawid Komorowski <dawidkomorowski@outlook.com>
2016-02-08 10:19:56 +0100

    Simple error handling when for some reason empty frames are captured.

--- data/VideoCapture.py
+++ data/VideoCapture.py
@@ -9,8 +9,18 @@ class VideoCapture(AbstractVideoCapture):
     def __init__(self):
         self.VIDEO_SIZE = (400, 300)
         self.cap = cv2.VideoCapture(0)  # 0 for camera
+        self.failedFramesLimit = 10
 
     def get_frame(self):
-        ret, frame = self.cap.read()
+        failedFrames = 0
+        while True:
+            ret, frame = self.cap.read()
+            if ret == True:
+                break
+
+            failedFrames = failedFrames + 1
+            if failedFrames > self.failedFramesLimit:
+                raise Exception("VideoCapture cannot receive stable video stream. Possible reason is camera driver issue.")
+
         frame = cv2.resize(cv2.flip(frame, 1), self.VIDEO_SIZE)  # vertical flip+ resize
         return frame
